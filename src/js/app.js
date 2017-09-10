e411c2f2689d99bb7f8d81e6e371f2028e4e3332
Ubuntu <ubuntu@ip-172-31-13-188.ap-southeast-2.compute.internal>
2017-09-10 05:47:47 +0000

    Reording app functions.

--- src/js/app.js
+++ src/js/app.js
@@ -4,22 +4,6 @@ App = {
 
   init: function() {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
     return App.initWeb3();
   },
 
@@ -35,82 +19,13 @@ App = {
     return App.initContract();
   },
 
-  getLog: function(callback) {
-	var crowdsaleInstance;
-
-	var logArray = [];
-	App.contracts.Crowdsale.deployed().then(function(instance){
-		crowdsaleInstance = instance;
-		let purchaseEvent = crowdsaleInstance.TokenPurchase({}, {fromBlock: 0, toBlock: 'latest'});
-		purchaseEvent.get((error, logs) => {
-			console.log('In getLog.');
-			logs.forEach(log => console.log(log.args))
-			logs.forEach(log => logArray.push(log))
-			callback(logArray);
-			//console.log(logArray[0])
-		});
-	});
-
-	return logArray;
-  },
-
-
-  watchLog: function() {
-	var crowdsaleInstance;
-
-	var logArray = [];
-	web3.eth.getAccounts(function(error, accounts) {
-		if (error) {
-			console.log(error);
-		}
-		
-		var account = accounts[3];
-		App.contracts.Crowdsale.deployed().then(function(instance){
-			crowdsaleInstance = instance;
-			let purchaseEvent = crowdsaleInstance.TokenPurchase({},{fromBlock: 0, toBlock: 'latest'});
-			purchaseEvent.watch((error, logs) => {
-				console.log(logs);
-				console.log(logArray);
-				logArray.push(logs);
-				console.log('Watch calling drawTable...')
-				App.drawTable(logArray);
-				App.createRegistry();
-			});
-		}).catch(function(err){
-			console.log(err.message);
-		});
-  	});
-  },
-
-  drawTable: function(logArray) {
-	  $(".log > tbody > tr").remove();
-
-	  for (var i = 0; i < logArray.length; i++) {
-		  console.log('In log array: ' + i)
-			var markup = "<tr><td>" 
-				+ i + "</td><td>"
-				+ logArray[i].args.purchaser + "</td><td>"
-				+ parseInt(logArray[i].args.amount) + "  </td><td>" 
-				+ web3.toWei(parseInt(logArray[i].args.value),'ether') 
-				+ "</td></tr>";
-			$(".log > tbody").append(markup);
-	  }
-
-  },
-
   initContract: function() {
 	$.getJSON('JackoCoinCrowdsale.json', function(data) {
 		//get the necessary contract artifact file and instnatiate it with truffle-contract.
 		var CrowdsaleArtifact = data;
 		App.contracts.Crowdsale = TruffleContract(CrowdsaleArtifact);
-
-		//set the provider for our contract.
 		App.contracts.Crowdsale.setProvider(App.web3Provider);
-		//App.getLog();
-		//App.watchLog();
-		//user our contract to retrieve and mark the adopted pets.
-		// Call UI refresh function
-		//return App.markAdopted();
+
 	}).then(function () {
 		$.getJSON('JackoCoin.json', function(data) {
 			var MintableTokenArtifact = data;
@@ -158,11 +73,55 @@ App = {
 			console.log(err.message);
 		});
 	});
-	//return App.watchLog();
   },
 
 
+  getLog: function(callback) {
+	var crowdsaleInstance;
+
+	var logArray = [];
+	App.contracts.Crowdsale.deployed().then(function(instance){
+		crowdsaleInstance = instance;
+		let purchaseEvent = crowdsaleInstance.TokenPurchase({}, {fromBlock: 0, toBlock: 'latest'});
+		purchaseEvent.get((error, logs) => {
+			console.log('In getLog.');
+			logs.forEach(log => console.log(log.args))
+			logs.forEach(log => logArray.push(log))
+			callback(logArray);
+			//console.log(logArray[0])
+		});
+	});
+
+	return logArray;
+  },
   
+  watchLog: function() {
+	var crowdsaleInstance;
+
+	var logArray = [];
+	web3.eth.getAccounts(function(error, accounts) {
+		if (error) {
+			console.log(error);
+		}
+		
+		var account = accounts[3];
+		App.contracts.Crowdsale.deployed().then(function(instance){
+			crowdsaleInstance = instance;
+			let purchaseEvent = crowdsaleInstance.TokenPurchase({},{fromBlock: 0, toBlock: 'latest'});
+			purchaseEvent.watch((error, logs) => {
+				console.log(logs);
+				console.log(logArray);
+				logArray.push(logs);
+				console.log('Watch calling drawTable...')
+				App.drawTable(logArray);
+				App.createRegistry();
+			});
+		}).catch(function(err){
+			console.log(err.message);
+		});
+  	});
+  },
+
   updateRegistry: function(transaction) {
 
 
@@ -211,6 +170,21 @@ App = {
 	});
   },
 
+  drawLog: function(logArray) {
+	  $(".log > tbody > tr").remove();
+
+	  for (var i = 0; i < logArray.length; i++) {
+		  console.log('In log array: ' + i)
+			var markup = "<tr><td>" 
+				+ i + "</td><td>"
+				+ logArray[i].args.purchaser + "</td><td>"
+				+ parseInt(logArray[i].args.amount) + "  </td><td>" 
+				+ web3.toWei(parseInt(logArray[i].args.value),'ether') 
+				+ "</td></tr>";
+			$(".log > tbody").append(markup);
+	  }
+
+  },
 
   drawRegistry: function(holders) {
 	  $(".registry > tbody > tr").remove();
